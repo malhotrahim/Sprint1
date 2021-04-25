244375194412823ee8bf5d7987c5cc4372967a4c
Aditya126 <43785455+Aditya126@users.noreply.github.com>
2021-04-25 18:45:16 +0530

    Add files via upload

---
+++ TesterClass/src/main/java/com/cg/ima/repository/IResourceRepository.java
@@ -0,0 +1,22 @@
+package com.cg.ima.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.cg.ima.entity.Resource;
+
+@Repository
+public interface IResourceRepository extends JpaRepository<Resource, Integer> {
+	
+
+	@Query("from Resource where category=:category and type=:type")
+	List<Resource> findByCategoryAndType(@Param("category") String category,@Param("type") String type);
+
+	@Query("from Resource where empId=:empid")
+	List<Resource> getByEmpId(@Param("empid") int empId);
+
+}
---
+++ TesterClass/src/main/java/com/cg/ima/repository/IUserRepository.java
@@ -0,0 +1,21 @@
+package com.cg.ima.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.cg.ima.entity.User;
+
+@Repository
+public interface IUserRepository extends JpaRepository<User, Integer> {
+
+	User findByUserId(String userId);
+
+	void deleteByUserId(String userId);
+
+	@Query("from User where userId=:userid and password=:pswrd")
+	User findByUser(@Param("userid") String userId,@Param("pswrd") String password);
+
+
+}
